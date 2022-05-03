2f1e3080c882ead60438b921e029bff02f132712
Godnelson <luizgatohd@gmail.com>
2022-05-03 16:41:28 -0300

    assinatura created

---
+++ src/main/java/com/senai/manflix/controller/AssinaturaController.java
@@ -0,0 +1,44 @@
+package com.senai.manflix.controller;
+
+import com.senai.manflix.model.Assinatura;
+import com.senai.manflix.repository.AssinaturaRepository;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+public class AssinaturaController {
+
+    AssinaturaRepository assinaturaRepository;
+
+    AssinaturaController(AssinaturaRepository assinaturaRepository){
+        this.assinaturaRepository = assinaturaRepository;
+    }
+
+    @GetMapping("/assinatura")
+    public List<Assinatura> getAll(){
+        return assinaturaRepository.findAll();
+    }
+
+    @GetMapping("/assinatura/{id}")
+    public Optional<Assinatura> getOne(@PathVariable(name = "id") Long id){
+        return assinaturaRepository.findById(id);
+    }
+
+    @PostMapping("/assinatura")
+    public Assinatura insertUser(@RequestBody Assinatura assinatura){
+        assinaturaRepository.save(assinatura);
+        return assinatura;
+    }
+
+    @PutMapping("/assinatura")
+    public Assinatura updateUser(@RequestBody Assinatura assinatura){
+        assinaturaRepository.save(assinatura);
+        return assinatura;
+    }
+
+    @DeleteMapping("/assinatura/{id}")
+    public String deleteUser(@PathVariable(name = "id") Long id){
+        assinaturaRepository.deleteById(id);
+        return "Deletado com Sucesso!";
+    }
+}
