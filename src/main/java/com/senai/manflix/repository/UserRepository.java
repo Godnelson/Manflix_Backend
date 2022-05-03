7047ef215f315a516ab671a3114ba643c7558abb
Godnelson <luizgatohd@gmail.com>
2022-05-03 16:43:19 -0300

    fix UserRepository

--- src/main/java/com/senai/manflix/repository/UserRepository.java
+++ src/main/java/com/senai/manflix/repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.senai.manflix.repository;
 import com.senai.manflix.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AssinaturaRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
